4e86a6928fd1265f7c0dd3752cd01087df1f0ebf
w00818794 <wenyuce@huawei.com>
2024-10-10 11:27:33 +0000

    Web cache max size
    Signed-off-by: w00818794 <wenyuce@huawei.com>

--- libcef/browser/alloy/alloy_content_browser_client.cc
+++ libcef/browser/alloy/alloy_content_browser_client.cc
@@ -306,6 +306,13 @@ void TransferVector(const std::vector<std::string>& source,
 }
 #endif
 
+#if defined(OHOS_CACHE)
+constexpr int64_t LARGE_CAPACITY_DEVICE_THRESHOLD = static_cast<int64_t>(100) * 1024 * 1024 * 1024;
+constexpr int64_t LARGE_CAPACITY_DEVICE_CACHE_SIZE = 100 * 1024 * 1024;
+constexpr int64_t SMALL_CAPACITY_DEVICE_CACHE_SIZE = 20 * 1024 * 1024;
+constexpr char WEB_CACHE_PATH[] = "/data/storage/e12/base/cache/web";
+#endif // defined(OHOS_CACHE)
+
 #if defined(OHOS_ARKWEB_EXTENSIONS)
 // The SpecialAccessFileURLLoaderFactory provided to the extension background
 // pages.  Checks with the ChildProcessSecurityPolicy to validate the file
@@ -2758,7 +2765,15 @@ bool AlloyContentBrowserClient::ConfigureNetworkContextParams(
       // Determined by DiskCache itself.
        network_context_params->http_cache_max_size = 0;
     } else {
-      network_context_params->http_cache_max_size = 20 * 1024 * 1024;
+      int64_t totalDiskSpace =
+        base::SysInfo::AmountOfTotalDiskSpace(base::FilePath(WEB_CACHE_PATH));
+      if (totalDiskSpace >= LARGE_CAPACITY_DEVICE_THRESHOLD) {
+        LOG(DEBUG) << "Set http cache max size to 100MB for large capacity device";
+        network_context_params->http_cache_max_size = LARGE_CAPACITY_DEVICE_CACHE_SIZE;
+      } else {
+        LOG(DEBUG) << "Set http cache max size to 20MB for small capacity device";
+        network_context_params->http_cache_max_size = SMALL_CAPACITY_DEVICE_CACHE_SIZE;
+      }
     }
   }
 #endif  // defined(OHOS_CACHE)
