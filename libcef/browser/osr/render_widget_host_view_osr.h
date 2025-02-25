2812aec38272acedf49b62ee8fb30f25afdc2d74
hwt00888022 <huwentao21@huawei.com>
2025-02-25 19:39:41 +0800

    修复结束滑动后冗余提频报点问题
    Signed-off-by: hwt00888022 <huwentao21@huawei.com>

--- libcef/browser/osr/render_widget_host_view_osr.h
+++ libcef/browser/osr/render_widget_host_view_osr.h
@@ -328,6 +328,10 @@ class CefRenderWidgetHostViewOSR
   void OnTouchDown();
 
   void StopBoosting();
+
+  void BoostingPreiodly();
+
+  void OnTouchMove();
 #endif
 
 #if defined(OHOS_INPUT_EVENTS)
@@ -724,6 +728,7 @@ class CefRenderWidgetHostViewOSR
 #if BUILDFLAG(IS_OHOS) && defined(OHOS_PERFORMANCE_JITTER)
   bool isBoosting_ = false;
   bool is_fling_ = false;
+  bool has_touch_point_ = false;
 #endif
 
 #ifdef OHOS_CLIPBOARD
