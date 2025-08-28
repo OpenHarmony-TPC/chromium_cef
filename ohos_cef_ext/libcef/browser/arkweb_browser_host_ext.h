5e33e6f25550e72b5bd6ce4136efb5deabcc81e0
diao-gaoyang <diaogaoyang@huawei.com>
2025-08-28 12:40:35 +0800

    prefetch追加修改
    Signed-off-by: diao-gaoyang <diaogaoyang@huawei.com>

--- ohos_cef_ext/libcef/browser/arkweb_browser_host_ext.h
+++ ohos_cef_ext/libcef/browser/arkweb_browser_host_ext.h
@@ -468,7 +468,7 @@ class ArkWebBrowserHostExtImpl : public ArkWebBrowserHostExt,
   int GetMediaPlaybackState() override;
 #endif  // BUILDFLAG(ARKWEB_MEDIA_POLICY)
 #if BUILDFLAG(ARKWEB_NO_STATE_PREFETCH)
-  void PrefetchPage(const PrefetchOptions prefetch_options) override;
+  void PrefetchPage(const OHOS::NWeb::PrefetchOptions prefetch_options) override;
 #endif  // ARKWEB_NO_STATE_PREFETCH
 #if BUILDFLAG(ARKWEB_MSGPORT)
   void CreateWebMessagePorts(std::vector<CefString>& ports) override;
