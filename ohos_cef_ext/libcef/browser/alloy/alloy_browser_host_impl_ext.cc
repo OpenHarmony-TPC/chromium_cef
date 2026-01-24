66fb82448f32f703f4b8758105e666eaa855558d
GoNewWay <duzhihao2@huawei.com>
2026-01-24 17:23:52 +0800

    fix
    Signed-off-by: GoNewWay <duzhihao2@huawei.com>

--- ohos_cef_ext/libcef/browser/alloy/alloy_browser_host_impl_ext.cc
+++ ohos_cef_ext/libcef/browser/alloy/alloy_browser_host_impl_ext.cc
@@ -1668,11 +1668,11 @@ void AlloyBrowserHostImplExt::ReportWindowStatus(bool first_view_ready) {
     }
 
     ResSchedClientAdapter::ReportWindowStatus(status, process_id, window_id_,
-                                            nweb_id_);
+                                              nweb_id_);
     if (!is_hidden_) {
       ResSchedClientAdapter::ReportScene(ResSchedStatusAdapter::WEB_SCENE_ENTER,
-                                          ResSchedSceneAdapter::VISIBLE,
-                                          nweb_id_);
+                                         ResSchedSceneAdapter::VISIBLE,
+                                         nweb_id_);
     }
   } else {
     LOG(ERROR) << "AlloyBrowserHostImplExt::ReportWindowStatus render_view_host is null";
