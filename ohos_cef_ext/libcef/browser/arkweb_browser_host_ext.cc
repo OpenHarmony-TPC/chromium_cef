95a17421720edf0f9fa2f3b675c7e3201b6fdd81
lingjinyue <lingyue2@huawei.com>
2026-02-02 09:14:13 +0800

    将修改挪到宏中
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- ohos_cef_ext/libcef/browser/arkweb_browser_host_ext.cc
+++ ohos_cef_ext/libcef/browser/arkweb_browser_host_ext.cc
@@ -602,10 +602,12 @@ void ArkWebBrowserHostExtImpl::PostTaskToUIThread(CefRefPtr<CefTask> task) {
 void ArkWebBrowserHostExtImpl::SetWebPreferences(
     const CefBrowserSettings& browser_settings) {
   UpdateBrowserSettings(browser_settings);
+#if BUILDFLAG(ARKWEB_LOGGER_REPORT)
   if (!GetWebContents()) {
+    LOG(ERROR)
+        << "ArkWebBrowserHostExtImpl::SetWebPreferences WebContents is nullptr";
     return;
   }
-#if BUILDFLAG(ARKWEB_LOGGER_REPORT)
   GetWebContents()->OnWebPreferencesChanged(settings_.usage_scenario);
 #else
   GetWebContents()->OnWebPreferencesChanged();
