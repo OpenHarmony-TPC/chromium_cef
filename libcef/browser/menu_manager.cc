299edd57fa90a109e4c68aa11fa0c1857bad391d
lingjinyue <lingyue2@huawei.com>
2026-04-15 09:30:35 +0800

    编译报错修改
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- libcef/browser/menu_manager.cc
+++ libcef/browser/menu_manager.cc
@@ -98,11 +98,9 @@ CefMenuManager::CefMenuManager(AlloyBrowserHostImpl* browser,
       browser_(browser),
       runner_(std::move(runner)),
 #if BUILDFLAG(ARKWEB_DEVTOOLS)
-      weak_ptr_factory_(this),
-      menu_manager_ext_(this) {
-#else
-      weak_ptr_factory_(this) {
+      menu_manager_ext_(this),
 #endif // BUILDFLAG(ARKWEB_DEVTOOLS)
+      weak_ptr_factory_(this) {
   DCHECK(web_contents());
   model_ = new CefMenuModelImpl(this, nullptr, false);
 }
